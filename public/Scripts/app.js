4d13f31eac908ed238d12e9f264da54f3f759e51
Samuel <Samueljambo81@gmail.com>
2021-04-25 11:04:49 -0400

    added Bootstarp support

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,10 @@
 //Custom Javascript
+//IIFE
+
+(function () {
+    function Start() {
+        console.log(`%c App Started...`, "font-size: 20px; color: blue; font-weight: bold;")
+    }
+
+    window.addEventListener("load", Start)
+})();
